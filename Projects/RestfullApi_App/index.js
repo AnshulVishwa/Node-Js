850662a6453915f4c440ec27b887b9db7efa272d
AnshulVishwa <anshulvishwa2205@gmail.com>
2024-12-11 17:48:52 +0530

    Adding Patch Method

--- Projects/RestfullApi_App/index.js
+++ Projects/RestfullApi_App/index.js
@@ -19,25 +19,38 @@ app.route("/")
         "id" : product.length+1,
         ...body
     } )
-    console.log(req.body)
+
     return res.json(new_product)
 } )
 
 // Route for Specific Document
 app.route("/products/:id")
 .get( ( req , res ) => {
-    return res.json( product.filter( ( pr ) => pr.id == req.params.id ) )
+    let prod = product.filter( ( pr ) => pr.id == req.params.id )
+    if( prod.length == 0 ) return res.json( { "Status" : "Not Found" } )
+    return res.json( prod )
 } )
 .put( ( req , res ) => {
     const id = req.params.id-1
     product[id] = {
         "id" : id+1,
         ...req.body
-    }
+    }  
     return res.json(product[id])
 } )
 .patch( ( req , res ) => {
-    
+    const id = req.params.id
+    const prod = product[id-1]
+    product[id-1] = {
+        id : prod.id,
+        name : req.body.name || prod.name,
+        price : req.body.price || prod.price,
+        expiryDate : req.body.expiryDate || prod.expiryDate
+    }
+
+    if( prod == product[id-1] ) return res.json( { "Status" : "No changes" } )
+
+    return res.json(product[id-1])
 } )
 
 // Establishing Server
