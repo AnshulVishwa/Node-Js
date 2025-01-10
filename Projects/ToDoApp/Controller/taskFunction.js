9d15466f863f4aa82ff5a70731752dd7613b9b07
AnshulVishwa <anshulvishwa2205@gmail.com>
2025-01-10 20:01:39 +0530

    Adding new Routes

--- Projects/ToDoApp/Controller/taskFunction.js
+++ Projects/ToDoApp/Controller/taskFunction.js
@@ -4,7 +4,7 @@ async function handlePostTask(req, res){
     const user = req.params.user
     const newtask = req.body.newtask
     if( !user ) return res.send("User is required")
-    const validateDuplicate = await TODO.findOne({"tasks" : 45}) 
+    const validateDuplicate = await TODO.findOne({"tasks" : { newtask }}) 
     console.log(validateDuplicate)
     if( validateDuplicate ) return res.status(400).json( { "error" : "Task already occured" } )
     const result = await TODO.findOneAndUpdate({"username" : user } , {$push : {tasks : newtask}})
@@ -20,7 +20,28 @@ async function handleGetAllTasks(req, res) {
     res.status(200).json(obj)
 }
 
+async function handleReqResCompletedTasks( req , res ){
+    const tasks = await TODO.findOne( { "username" : req.params.user } )
+    if( !tasks ) return res.status(404).json( { "msg" : "User not found" } )
+    if( tasks.CompletedTasks.length == 0 ) return res.status(400).json({"msg" : "No Tasks"})
+    let obj = {}
+    for( let i = 0 ; i < tasks.CompletedTasks.length ; i++ ) obj[`Task ${i+1} ✔️`] = tasks.CompletedTasks[i] 
+    res.status(200).json(obj)
+}
+
+async function PostCompletedTasks( req , res ) {
+    const user = await TODO.findOne({ "username" : req.params.user })
+    const task = user.tasks[req.params.task]
+    if( !task ) res.status(400).json({"status" : "Task not Found"})
+    await TODO.updateOne({ "username" : req.params.user } , { $pull : { "tasks" : `${task}` } })
+    const result = await TODO.updateOne({ "username" : req.params.user } , { $push : { "CompletedTasks" : task } })
+    if( !result ) res.status(400).json( { "status" : "Error Occured" } )
+    res.json({"status" : "Tasks Updated"})
+}
+
 module.exports = {
     handleGetAllTasks,
-    handlePostTask
+    handlePostTask,
+    handleReqResCompletedTasks,
+    PostCompletedTasks
 };
--- Projects/ToDoApp/Routes/task.js
+++ Projects/ToDoApp/Routes/task.js
@@ -1,14 +1,15 @@
 const express = require("express");
-const { handleGetAllTasks, handlePostTask } = require("../Controller/taskFunction");
+const { handleGetAllTasks, handlePostTask , handleReqResCompletedTasks , PostCompletedTasks } = require("../Controller/taskFunction");
 const TaskRoute = express.Router();
 
 TaskRoute.get("/:user", handleGetAllTasks);
+TaskRoute.get("/:user/completedTasks" , handleReqResCompletedTasks)
 TaskRoute.post("/:user", handlePostTask);
+TaskRoute.post("/:user/:task(\\d+)", PostCompletedTasks);
+
 
 module.exports = TaskRoute
 
-// TaskRoute.get("/completedTasks")
 // TaskRoute.get("/deletedTask")
-
 // TaskRoute.post("/newTask")
 // TaskRoute.post("/taskCompleted")
