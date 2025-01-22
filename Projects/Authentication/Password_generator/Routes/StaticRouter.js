5ab03d4a65d2097d7575d3ffe9c18aebe6f85ea0
Anshul Vishwakarma <165347635+AnshulVishwa@users.noreply.github.com>
2025-01-22 07:28:59 +0530

    Delete Projects/Authentication directory

--- Projects/Authentication/Password_generator/Routes/StaticRouter.js
+++
@@ -1,5 +0,0 @@
-const express = require("express")
-const StaticRouter = express.Router()
-
-
-StaticRouter.get( "/" , ( req , res ) => res.render("home") )
--- Projects/Authentication/connect.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require("mongoose")
-
-async function connectToMongoDB( url ) {
-    return await mongoose.connect(url)
-}
-
-module.exports = {connectToMongoDB}
