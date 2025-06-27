95e46a4b4be8580003048fc988ec6dda90f53b05
AnshulVishwa <anshulvishwa2205@gmail.com>
2025-06-27 21:58:12 +0530

    Revising Modues

---
+++ Modules/anshul.js
@@ -0,0 +1,5 @@
+function getName(){
+    return "Anshul"
+}
+
+module.exports = { getName }
--- Modules/vidhi.js
+++ Modules/vidhi.js
@@ -1,2 +1,2 @@
-const fs = require("fs")
-
+const { getName } = require("./anshul");
+console.log(getName())
